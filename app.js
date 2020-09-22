6b949e10f35ab7936e68776329e69d3eb37f4b45
eeds90 <eddie.diaz90@icloud.com>
2020-09-22 13:07:16 -0500

    edwin diaz

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
-async function getBook() {
+async function getBook(userInput) {
   try {
-    const reponse = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${alchemist}`)
+    const response = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${userInput}`)
     console.log(response)
-  } catch (e) {
-    console.log(e)
+  } catch (error) {
+    console.log(`Error: ${error}`)
   }
 }
 getBook()
+
+const form = document.querySelector("#search-form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const inputValue = document.querySelector("#search").value;
+  document.querySelector("#search").value = "";
+  fetchData(inputValue);
+})
