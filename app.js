f7cfe70cb2569998154be0abd3debd19a13e5bd6
eeds90 <eddie.diaz90@icloud.com>
2020-09-22 19:48:10 -0500

    edwin diaz

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
 async function getBook(userInput) {
   try {
     const response = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${userInput}`)
-    console.log(response)
+    console.log(response.data.items[0].searchInfo)
+    const data= response.data.items[0].searchInfo
   } catch (error) {
     console.log(`Error: ${error}`)
   }
 }
 getBook()
 
-button.addEventListener('click', async () => {
-  let userInput = input.value;
-  const response = await
-  axios.get(`${BASE_URL}/search/${userInput}`); 
-console.log(response.title.results[0]);
-getBook(response.title.results[0])
-});
+// Created a variable to grab my form
+const form = document.querySelector('#Toolbar');
+form.addEventListener("submit", (e) => {  
+  e.preventDefault();
+  const inputValue = document.querySelector('#search').value; // ON my HTML document grag my input ID
+  document.querySelector('#search').value = "Title";
+  getBook(inputValue)
+}); 
 
-const button = document.querySelector('.submit-button')
+let resultDiv = document.querySelector('#Toolbar');
+function displayResults(e) {
+  let results = e.map((data) => {
+    const image = document.createElement('img');
+    image.setAttribute('src, data.')
+  })
+} 
